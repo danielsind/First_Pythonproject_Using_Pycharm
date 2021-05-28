f588686c54c7f25236c077ea40e7852297619644
danielsind <danielsindet@gmail.com>
2021-05-28 22:16:40 +0300

    Attempted to run small code

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 print("Hello World am trying to learn using Pycharm")
+print("Atleast now i can run code on pycharm")
+string = input("Enter the language you are using ")
+print("Your language is " + string)
